9f4b986dee1f0c6b9ba6ffb5174eafcd2e697e76
webQbe <webcubesolutionslk@gmail.com>
2025-01-21 20:06:16 +0530

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import { Header } from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Header />
     </div>
   );
 }
