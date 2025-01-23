c1aed83ad9afbd4b3d98cf0f9d9ea2542d586fad
webQbe <webcubesolutionslk@gmail.com>
2025-01-23 14:19:30 +0530

    Styled transaction element border based on income or expense

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,8 +4,9 @@ export const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li className="minus">
-        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+        {transaction.text} 
+            <span>{sign}${Math.abs(transaction.amount)}</span> 
         <button className="delete-btn">x</button>
     </li>
   )
