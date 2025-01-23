2da794a51aa0e050545bc68dd17c208928d053cf
webQbe <webcubesolutionslk@gmail.com>
2025-01-23 14:06:53 +0530

    Created Transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Transaction } from './Transaction';
 import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
@@ -8,10 +9,10 @@ export const TransactionList = () => {
     <> 
         <h3>History</h3>
         <ul className="list">
-            {transactions.map(transaction => (<li className="minus">
-                {transaction.text} <span>-$400</span>
-                <button className="delete-btn">x</button>
-            </li>))}
+            {transactions.map(transaction => (
+                /* Output each item to component with a key  */
+                <Transaction key={transaction.id} transaction={transaction} />
+              ))}
         </ul>
     </>
   )
